9f10d8d03c462f8a882a2f1ff5cf6de791d1f7b3
Juan La Jara <juanlajara@gmail.com>
2013-03-06 13:44:26 -0500

    using bower dependencies

--- jquery.pub-sub-validate.js
+++ jquery.pub-sub-validate.js
@@ -14,47 +14,6 @@
 
 /*jshint laxcomma:true, smarttabs:true, forin:true, noarg:true, noempty:true, eqeqeq:true, laxbreak:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
 
-//Polyfills
-(function () {
-	"use strict";
-	// Object Create Polyfill
-	if ( typeof Object.create !== 'function' ) {
-		Object.create = function( obj ) {
-			function F() {}
-			F.prototype = obj;
-			return new F();
-		};
-	}
-
-	// Array.every Polyfill
-	if (!Array.prototype.every) {
-		Array.prototype.every = function(fun /*, thisp */) {
-			
-			if (this === null) {
-				throw new TypeError();
-			}
-			
-			var t = Object(this);
-			var len = t.length >>> 0;
-
-			if (typeof fun !== "function") {
-				throw new TypeError();
-			}
-			
-			var thisp = arguments[1];
-			for (var i = 0; i < len; i++) {
-				if (i in t && !fun.call(thisp, t[i], i, t)) {
-					return false;
-				}
-			}
-			
-			return true;
-		};
-	}
-
-} )();
-
-
 /* jQuery Tiny Pub/Sub - v0.7 - 10/27/2011
  * http://benalman.com/
  * Copyright (c) 2011 "Cowboy" Ben Alman; Licensed MIT, GPL */
