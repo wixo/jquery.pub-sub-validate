299c91db3a3b47bf2e8bfc9f683d83bfb7f7d163
Juan La Jara <juanlajara@gmail.com>
2013-03-10 18:52:25 -0500

    Add custom error message and documentation

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-jquery.pub-sub-validate.js
+jquery.pslidate.js
 =====
 
 Pub sub validation for your bootstrap forms!
 
-
-Will document it next time
+Usage
 =====
+
+So, you first build your markup of your form, following the Bootstrap style.
+Then you define the validation rules in the markup of your controls like: `<input data-pslidate="text" type="text" ... >`
+You also can define your custom error messages with the `pslerror` data attribute: `<input data-pslerror="Please write your address" type="text" ... >`
+
+Finally you can validate your form
+
+```JavaScript
+$('#my-form').on('submit', function () {
+    var isValid = $(this).pslidate();
+
+    if( isValid ) {
+        console.log('ajax call maybe?');
+    }
+
+});
+```
+
+Why?
+====
+
+Well, I just wanted to liberate the code I use in my day to day work.
+Maybe it will get better on every commit.
--- jquery.pub-sub-validate.js
+++ jquery.pub-sub-validate.js
@@ -1,233 +1,249 @@
 //
 //
-//    jQuery.pub-sub-validate 0.0.1
+//    jQuery.psublidate 0.0.1
 //    Validate your forms in a pub sub way!
-//    assuming jQuery and TwBootstrap, libraries free version sometime around 2016
+//
+//    Dependencies:
+//    - E5Shim for oldIE
+//    - jQuery
+//    - Bootstrap forms markup
+//    - jQuery-tiny-pubsub
 //
 //
-
-//  lots of constraints: i.e. validation rules live in the markup as a data tag. be careful.
-
+//  Lots of constraints: i.e. validation rules live in the markup as a data tag. be careful.
+//
 //  TODO: Find a really cool name.
-//  TODO: Define dependencies instead of polyfills
 //  TODO: Add multiple checkboxes, radio buttons and multiple radio buttons validations
+//
+//
 
 /*jshint laxcomma:true, smarttabs:true, forin:true, noarg:true, noempty:true, eqeqeq:true, laxbreak:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
 
 // Pubsub Validate
 
 (function ($, window, document, undefined) {
-	"use strict";
+    "use strict";
 
-	var FV = (function() {
+    var PSL = (function() {
 
-		var controlsStatuses = []
-		  , isValid          = false
-		  , init
-		  , updateFormStatus
-		  , updateControlStatuses
-		  , updateControlClass
-		  , updateControlMessage
-		  , validateControl
-		  , validateFilter
-		  , validateSelect
-		  , validateCheckbox
-		  ;
+        // Statuses in an Array
+        var controlsStatuses       = []
+        // Are all validated inputs valid?
+          , isValid                = false
+        // Main flow
+          , init
+          , updateFormStatus
+          , updateControlStatuses
+          , updateControlClass
+          , updateControlMessage
+          , validateControl
+          , validateFilter
+          , validateSelect
+          , validateCheckbox
+          ;
 
-		init = function ( options ) {
-			var self   = this;
+        init = function ( options ) {
+            // This is the form, so is self
+            var self   = this;
 
-			this.$form = $(options.form);
+            this.$form = $(options.form);
 
-			//TODO: implement no mark up based version
-			this.controls = this.$form.find('[data-validate]');
-			this.config   = options.config;
+            //TODO: implement no mark up based version
+            this.controls = this.$form.find('[data-pslidate]');
+            this.config   = options.config;
 
-			$.subscribe( 'valid-form', updateFormStatus );
+            $.subscribe( 'valid-form' , updateFormStatus );
 
-			$.each( self.controls, function ( index, control ) {
-				var rule
-				  , validation
-				  , publishOptions
-				  ;
+            $.each( self.controls, function ( index, control ) {
+                var rule
+                  , validation
+                  , publishOptions
+                  ;
 
-				control        = $(control);
-				rule           = control.data('validate');
-				validation     = self.config.rules[rule];
-				publishOptions = {
-					status  : validateControl.apply( control, [{rule: rule, config: self.config}] )
-				  , index   : index
-				  , control : control
-				  , config  : self.config
-				};
+                control        = $(control);
+                rule           = control.data('pslidate');
+                validation     = self.config.rules[rule];
+                publishOptions =
+                    { status  : validateControl.apply( control, [{rule: rule, config: self.config}] )
+                    , index   : index
+                    , control : control
+                    , config  : self.config
+                    };
 
-				$.subscribe( 'control-' + index, updateControlStatuses );
-				$.publish( 'control-' + index, publishOptions );
+                $.subscribe( 'control-' + index, updateControlStatuses );
 
-				if ( validation === 'filter' ) {
+                $.publish( 'control-' + index, publishOptions );
 
-					control.on('keyup paste blur', function () {
+                if ( validation === 'filter' ) {
 
-						$.publish( 'control-' + index, {
-							status  : validateControl.apply( control, [{rule: rule, config: self.config}] )
-						  , index   : index
-						  , control : control
-						  , config  : self.config
-						} );
-					} );
+                    control.on('keyup paste blur', function () {
 
-				} else if ( validation === 'select' || validation === 'checkbox' ) {
+                        $.publish( 'control-' + index,
+                            { status  : validateControl.apply( control, [{rule: rule, config: self.config}] )
+                            , index   : index
+                            , control : control
+                            , config  : self.config
+                            }
+                        );
+                    } );
 
-					control.on('change', function () {
-						$.publish( 'control-' + index, {
-							status  : validateControl.apply( control, [{rule: rule, config: self.config}] )
-						  , index   : index
-						  , control : control
-						  , config  : self.config
-						} );
-					} );
+                } else if ( validation === 'select' || validation === 'checkbox' ) {
 
-				} else {
+                    control.on('change', function () {
+                        $.publish( 'control-' + index,
+                            { status  : validateControl.apply( control, [{rule: rule, config: self.config}] )
+                            , index   : index
+                            , control : control
+                            , config  : self.config
+                            }
+                        );
+                    } );
 
-					throw new Error('PubSubValidate: There is no validation for ' + rule + ', please review your data-validate rules');
+                } else {
 
-				}
+                    throw new Error('PubSubValidate: There is no validation for ' + rule + ', please review your data-validate rules');
 
-			} );
+                }
 
-			return isValid;
-		};
+            } );
 
-		updateFormStatus = function (e, options) {
-			isValid = options.isValid;
-		};
+            return isValid;
+        };
 
-		updateControlStatuses = function (e, options) {
-			var control  = options.control
-			  , message  = ( options.status !== true ) && options.config.messages[options.status]
-			  , messageContainer = control.parent().find('p.help-inline')
-			  , isValid;
+        updateFormStatus = function (e, options) {
+            isValid = options.isValid;
+        };
 
-			controlsStatuses[options.index] = options.status;
+        updateControlStatuses = function (e, options) {
+            var control          = options.control
+              , message          = ( options.status !== true ) && options.config.messages[options.status]
+              , messageContainer = control.parent().find('p.help-inline')
+              , isValid
+              ;
 
-			updateControlMessage( {
-				messageContainer: messageContainer
-			  , text : message || ''
-			} );
+            controlsStatuses[options.index] = options.status;
 
-			updateControlClass( {
-				control: control
-			  , type   : message && 'error'
-			  , config : options.config
-			} );
+            updateControlMessage(
+                { messageContainer : messageContainer
+                    // Use control custom error message or control default error message or none
+                , text             : control.data('pslerror') || message || ''
+                }
+            );
 
-			isValid = controlsStatuses.every( function( element ) {
-				return element === true;
-			} );
+            updateControlClass( {
+                control: control
+              , type   : message && 'error'
+              , config : options.config
+            } );
 
-			$.publish('valid-form', { isValid: isValid });
+            isValid = controlsStatuses.every( function( element ) {
+                return element === true;
+            } );
 
-		};
+            $.publish('valid-form', { isValid: isValid });
 
-		updateControlClass = function ( options ) {
-			var $controlHolder = options.control.parent().parent()
-			  , errorClass     = options.config.errorClass;
+        };
 
-			if ( options.type === 'error' ) {
-				$controlHolder.addClass( errorClass );
-			} else {
-				$controlHolder.removeClass( errorClass );
-			}
+        updateControlClass = function ( options ) {
+            var $controlHolder = options.control.parent().parent()
+              , errorClass     = options.config.errorClass;
 
-		};
+            if ( options.type === 'error' ) {
+                $controlHolder.addClass( errorClass );
+            } else {
+                $controlHolder.removeClass( errorClass );
+            }
 
-		updateControlMessage = function ( options ) {
-			options.messageContainer.text( options.text );
-		};
+        };
 
-		validateControl = function ( options ) {
-			var $this             = $(this)
-			  , rule              = options.rule
-			  , validation        = options.config.rules[rule]
-			  , validationOptions = {
-			    control : $this
-			  , config  : options.config
-			  , filter  : options.config.filters[rule]
-			  , rule    : rule
-			}
-			  ;
+        updateControlMessage = function ( options ) {
+            options.messageContainer.text( options.text );
+        };
 
-			if ( validation === 'filter' ) {
+        validateControl = function ( options ) {
+            var $this             = $(this)
+              , rule              = options.rule
+              , validation        = options.config.rules[rule]
+              , validationOptions =
+                { control : $this
+                , config  : options.config
+                , filter  : options.config.filters[rule]
+                , rule    : rule
+                }
+              ;
 
-				return validateFilter( validationOptions );
+            if ( validation === 'filter' ) {
 
-			} else if ( validation === 'select' ) {
+                return validateFilter( validationOptions );
 
-				return validateSelect( validationOptions );
+            } else if ( validation === 'select' ) {
 
-			} else if ( validation === 'checkbox' ) {
+                return validateSelect( validationOptions );
 
-				return validateCheckbox( validationOptions );
+            } else if ( validation === 'checkbox' ) {
 
-			}
+                return validateCheckbox( validationOptions );
 
-		};
+            }
 
-		validateFilter = function ( options ) {
-			var $val   = options.control.val()
-			  , filter = options.filter || /[]/;
+        };
 
-			return filter.test( $val ) || options.rule;
-		};
+        validateFilter = function ( options ) {
+            var $val   = options.control.val()
+              , filter = options.filter || /.*/
+              ;
 
-		validateSelect = function ( options ) {
-			var $val = options.control.val();
+            return filter.test( $val ) || options.rule;
+        };
 
-			return $val !== '' || options.rule;
-		};
+        validateSelect = function ( options ) {
+            var $val = options.control.val();
 
-		validateCheckbox = function ( options ) {
-			var $val = options.control.prop('checked');
+            return $val !== '' || options.rule;
+        };
 
-			return $val || options.rule;
-		};
+        validateCheckbox = function ( options ) {
+            var $val = options.control.prop('checked');
 
-		return {
-			init : init
-		};
+            return $val || options.rule;
+        };
 
-	} )();
+        return {
+            init : init
+        };
 
-	$.fn.validate = function () {
-		var fv = Object.create( FV );
-		// debug( $.fn.validate.config );
-		return fv.init( {form: this, config: $.fn.validate.config} );
-	};
+    } )();
 
-	$.fn.validate.config = {
-		filters : {
-			text      : /^([a-záéíóúÁÉÍÓÚA-Z \-])+$/
-		  , number    : /^([0-9\-])+$/
-		  , email     : /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
-		  , notEmpty  : /^\s*\S.*$/
-		}
-	  , messages : {
-				text      : "Escriba un texto válido."
-			  , number    : "Escriba un número válido."
-			  , email     : "Escriba un email válido."
-			  , notEmpty  : "No se puede dejar en blanco."
-			  , select    : "Elija una opción."
-			  , checkbox  : "Elija una opción"
-			}
-		  , rules  : {
-				text      : "filter"
-			  , number    : "filter"
-			  , email     : "filter"
-			  , notEmpty  : "filter"
-			  , select    : "select"
-			  , checkbox  : "checkbox"
-			}
-		  , errorClass : 'error'
-		};
+    $.fn.pslidate = function () {
+        var psl = Object.create( PSL );
+        // debug( $.fn.pslidate.config );
+        return psl.init( {form: this, config: $.fn.pslidate.config} );
+    };
+
+    $.fn.pslidate.config =
+        { filters :
+          { text      : /^([a-záéíóúÁÉÍÓÚA-Z \-])+$/
+          , number    : /^([0-9\-])+$/
+          , email     : /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
+          , notEmpty  : /^\s*\S.*$/
+          }
+        , messages :
+          { text      : "Write a valid text."
+          , number    : "Write a valid number."
+          , email     : "Write a valid e-mail address."
+          , notEmpty  : "Can not be empty."
+          , select    : "Choose an option."
+          , checkbox  : "Choose an option"
+          }
+        , rules  :
+          { text      : "filter"
+          , number    : "filter"
+          , email     : "filter"
+          , notEmpty  : "filter"
+          , select    : "select"
+          , checkbox  : "checkbox"
+          }
+        , errorClass : 'error'
+        };
 
 } )( window.jQuery, window, document );
